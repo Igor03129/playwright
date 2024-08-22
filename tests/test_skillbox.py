9e85af9e020a08cf0b29033503281540f35b15f7
Igor <1609528843@my.lerna.md>
2024-08-22 16:06:57 +0300

    does not work

--- tests/test_skillbox.py
+++ tests/test_skillbox.py
@@ -5,3 +5,4 @@ def test_example(config, home_page, professions_page):
     home_page.load(config.get("url"))
     home_page.see_professions()
     expect(professions_page.qa_enginner_link).to_be_visible()
+    # does not work
